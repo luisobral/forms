6fff0132b1d253cae50d1350572dbc29f2a4d206
Nluisobral <luisobral09@gmail.com>
2020-09-09 15:24:40 -0300

    start

---
+++ Shared/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Forms.Shared
+{
+    public class Usuario
+    {
+        public string Title { get; set;}
+        public string First { get; set; }
+        public string Last { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
